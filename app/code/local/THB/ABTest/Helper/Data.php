39584974168e844972245c137b9805221c9e93e7
Tony Holdstock-Brown <tonyhb@gmail.com>
2013-03-24 01:50:49 -0400

    Completely refactors the setting of cohorts.
    In this commit we've moved all cohort/variation management from the observer
    model into a visitor helper. This has the following benefits:
    
    + We've no longer got one large method for setting variations for users
    + The Observer model only manages event-related information
    + The new visitor helper has a much nicer API and much better functionality
      for managing visitor cohorts
    + Changing session/variation storage to cookies will be easier
    + Code is much less repetitive
    + Tracking hits and conversions is much easier

--- app/code/local/THB/ABTest/Helper/Data.php
+++ app/code/local/THB/ABTest/Helper/Data.php
@@ -2,5 +2,63 @@
 
 class THB_ABTest_Helper_Data extends Mage_Core_Helper_Data {
 
+    /**
+     * Stores whether an A/B test is running
+     *
+     * @since 0.0.1
+     *
+     * @var bool
+     */
+    protected static $_is_running = FALSE;
+
+    /**
+     * Returns whether any A/B tests are running or not.
+     *
+     * @since 0.0.1
+     *
+     * @api
+     * @return array
+     */
+    public static function getIsRunning()
+    {
+        return self::$_is_running;
+    }
+
+    /**
+     * Stores an array of all active tests
+     *
+     * @since 0.0.1
+     *
+     * @var array
+     */
+    protected static $_active_tests = NULL;
+
+    /**
+     * Returns an array of all active tests and their variations. We use this 
+     * method instead of the test collection's method because this cache's the 
+     * query result, optimising performance of the A/B test by decreasing 
+     * queries.
+     *
+     * @since 0.0.1
+     *
+     * @api
+     * @return array
+     */
+    public static function getActiveTests()
+    {
+        # We must already have initialised active tests, because it wouldn't be 
+        # NULL otherwise. We can safely return our property
+        if (self::$_active_tests !== NULL)
+            return self::$_active_tests;
+
+        self::$_active_tests = Mage::getModel('abtest/test')->getCollection()->getActiveTests();
+
+        if (self::$_active_tests != array())
+        {
+            self::$_is_running = TRUE;
+        }
+
+        return self::$_is_running;
+    }
 
 }
