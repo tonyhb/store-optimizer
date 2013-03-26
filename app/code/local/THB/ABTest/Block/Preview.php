b99d29e0bc21e390c5d39475263f9e613e3a96be
Tony Holdstock-Brown <tonyhb@gmail.com>
2013-03-26 12:01:16 +0000

    Adds extra files for previewing running test's XML layout updates

---
+++ app/code/local/THB/ABTest/Block/Preview.php
@@ -0,0 +1,11 @@
+<?php
+
+class THB_ABTest_Block_Preview extends Mage_Core_Block_Template
+{
+
+    public function __construct()
+    {
+        $this->setTemplate('abtest/preview.phtml');
+    }
+
+}
