45286245410e035ad86b8414d81ca8543b797444
Tony Holdstock-Brown <tonyhb@gmail.com>
2013-06-15 01:41:04 -0400

    Fixes a bug which caused visitors to be logged in the admin panel

--- app/code/community/THB/ABTest/Model/Overrides/Design.php
+++ app/code/community/THB/ABTest/Model/Overrides/Design.php
@@ -3,22 +3,33 @@
 class THB_ABTest_Model_Overrides_Design extends Mage_Core_Model_Design_Package
 {
 
-    public function __construct()
-    {
-        # First, we need to assign variations. This is because if 
-        # a visitor hasn't been to our website yet, they're NOT in 
-        # a cohort - this gets called first. If the visitor gets 
-        # put into a cohort with a different theme, the website's 
-        # design is going to change after the first page view. This 
-        # call ensures we don't get this...
-         Mage::helper('abtest/visitor')->assignVariations();
-    }
-
+    /**
+     * Gets the overall theme package name for the website. 
+     *
+     * This has been overridden to change the frontend package on 
+     * a variation-by-variation basis.
+     *
+     * Also, this assigns the users to variations because it is the first method 
+     * to be called in each of the design overrides, and a user needs to be in 
+     * a cohort before modifications can be made.
+     *
+     * We CAN'T put assignVariations() in the __construct method because, if so, 
+     * admin users trigger visits when browsing the admin side, and the getArea() 
+     * method isn't accurate when called in __construct to prevent this.
+     *
+     */
     public function getPackageName()
     {
         if (null === $this->_name) {
             # Are we testing or previewing a new theme?
             if ($this->getArea() != 'adminhtml') {
+                # First, we need to assign variations. This is because if 
+                # a visitor hasn't been to our website yet, they're NOT in 
+                # a cohort - this gets called first. If the visitor gets 
+                # put into a cohort with a different theme, the website's 
+                # design is going to change after the first page view. This 
+                # call ensures we don't get this...
+                Mage::helper('abtest/visitor')->assignVariations();
 
                 # Are we previewing a theme?
                 if ($preview = Mage::helper('abtest/visitor')->getPreview()) {
